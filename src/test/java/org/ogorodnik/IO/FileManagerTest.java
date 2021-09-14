921982348debb97eb5c8298bd66a90a33b50d753
alexone <persyak@ukr.net>
2021-09-14 16:28:38 +0300

    Added several sections: - Before section to create test data; - After section to remove test data; - methods to compare file contents;
    Refactored tests.

--- src/test/java/org/ogorodnik/IO/FileManagerTest.java
+++ src/test/java/org/ogorodnik/IO/FileManagerTest.java
@@ -1,9 +1,12 @@
 package org.ogorodnik.IO;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,25 +26,38 @@ public class FileManagerTest {
         new File("TestData/DataForMove/from").mkdir();
         new File("TestData/DataForMove/to").mkdir();
         new File("TestData/DataForMove/from/InnerFolder").mkdir();
-        createFilesWithContent("TestData/DataForCount/File1.txt");
-        createFilesWithContent("TestData/DataForCount/File2.txt");
-        createFilesWithContent("TestData/DataForCount/File3.txt");
-        createFilesWithContent("TestData/DataForCount/InnerDirectory1/File4.txt");
-        createFilesWithContent("TestData/DataForCopy/from/File1.txt");
-        createFilesWithContent("TestData/DataForCopy/from/File2.txt");
-        createFilesWithContent("TestData/DataForCopy/from/InnerFolder/File3.txt");
-        createFilesWithContent("TestData/DataForMove/from/File1.txt");
-        createFilesWithContent("TestData/DataForMove/from/InnerFolder/File1.txt");
+        createFilesWithContent("TestData/DataForCount/File1.txt", "TestData/DataForCount/File1.txt file");
+        createFilesWithContent("TestData/DataForCount/File2.txt", "TestData/DataForCount/File2.txt");
+        createFilesWithContent("TestData/DataForCount/File3.txt", "TestData/DataForCount/File3.txt");
+        createFilesWithContent("TestData/DataForCount/InnerDirectory1/File4.txt", "TestData/DataForCount/InnerDirectory1/File4.txt");
+        createFilesWithContent("TestData/DataForCopy/from/File1.txt", "TestData/DataForCopy/from/File1.txt");
+        createFilesWithContent("TestData/DataForCopy/from/File2.txt", "TestData/DataForCopy/from/File2.txt");
+        createFilesWithContent("TestData/DataForCopy/from/InnerFolder/File3.txt", "TestData/DataForCopy/from/InnerFolder/File3.txt");
+        createFilesWithContent("TestData/DataForMove/from/File1.txt", "TestData/DataForMove/from/File1.txt");
+        createFilesWithContent("TestData/DataForMove/from/InnerFolder/File1.txt", "TestData/DataForMove/from/InnerFolder/File1.txt");
     }
 
-    private void createFilesWithContent(String path) throws IOException {
+    private void createFilesWithContent(String path, String text) throws IOException {
         new File(path).createNewFile();
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
-        bufferedWriter.write("This is a test file");
+        bufferedWriter.write(text);
         bufferedWriter.newLine();
-        bufferedWriter.write("It's made to test FileManager");
+        bufferedWriter.write(text);
         bufferedWriter.flush();
         bufferedWriter.close();
+    }
+
+    private List<String> createListOfFileContent(String file) throws IOException {
+        List<String> list = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while(bufferedReader.ready()){
+            list.add(bufferedReader.readLine());
+        }
+        return list;
+    }
+
+    private boolean checkIfTwoFilesAreEqual(List<String> list1, List<String> list2) throws IOException {
+        return list1.equals(list2);
     }
 
     @Test
@@ -86,24 +102,73 @@ public class FileManagerTest {
     public void testCopyFile() throws IOException {
         FileManager.copy("./TestData/DataForCopy/from/File1.txt", "./TestData/DataForCopy/to/");
         assertTrue((new File("./TestData/DataForCopy/to/File1.txt")).exists());
+        assertTrue(checkIfTwoFilesAreEqual(createListOfFileContent("./TestData/DataForCopy/from/File1.txt"),
+                createListOfFileContent("./TestData/DataForCopy/to/File1.txt")));
     }
 
     @Test
     public void testCopyDirectory() throws IOException {
-        FileManager.copy(".//TestCopy//from//sourceFolder", ".//TestCopy//to//destinationFolder//");
-        assertTrue((new File(".//TestCopy//to//destinationFolder//File1.rtf")).exists());
-        assertTrue((new File(".//TestCopy//to//destinationFolder//innerFolder")).exists());
-        assertTrue((new File(".//TestCopy//to//destinationFolder//innerFolder")).isDirectory());
-        assertTrue((new File(".//TestCopy//to//destinationFolder//innerFolder//File1.rtf")).exists());
+        FileManager.copy("./TestData/DataForCopy/from", "./TestData/DataForCopy/to");
+        assertTrue((new File("./TestData/DataForCopy/to/File1.txt")).exists());
+        assertTrue((new File("./TestData/DataForCopy/to/File2.txt")).exists());
+        assertTrue((new File("./TestData/DataForCopy/to/InnerFolder")).exists());
+        assertTrue((new File("./TestData/DataForCopy/to/InnerFolder")).isDirectory());
+        assertTrue((new File("./TestData/DataForCopy/to/InnerFolder/File3.txt")).exists());
+        assertTrue(checkIfTwoFilesAreEqual(createListOfFileContent("./TestData/DataForCopy/from/File1.txt"),
+                createListOfFileContent("./TestData/DataForCopy/to/File1.txt")));
+        assertTrue(checkIfTwoFilesAreEqual(createListOfFileContent("./TestData/DataForCopy/from/File2.txt"),
+                createListOfFileContent("./TestData/DataForCopy/to/File2.txt")));
+        assertTrue(checkIfTwoFilesAreEqual(createListOfFileContent("./TestData/DataForCopy/from/InnerFolder/File3.txt"),
+                createListOfFileContent("./TestData/DataForCopy/to/InnerFolder/File3.txt")));
+        assertFalse(checkIfTwoFilesAreEqual(createListOfFileContent("./TestData/DataForCopy/from/File1.txt"),
+                createListOfFileContent("./TestData/DataForCopy/to/File2.txt")));
     }
 
     @Test
     public void testMove() throws IOException {
-        assertTrue((new File(".//TestCopy//from//sourceToMove")).exists());
-        assertTrue((new File(".//TestCopy//from//sourceToMove//File1.rtf")).exists());
-        FileManager.move(".//TestCopy//from//sourceToMove", ".//TestCopy//to//destinationOfMovedFolder");
-        assertFalse((new File(".//TestCopy//from//sourceToMove")).exists());
-        assertFalse((new File(".//TestCopy//from//sourceToMove//File1.rtf")).exists());
-        assertTrue((new File(".//TestCopy//to//destinationOfMovedFolder")).exists());
+        assertTrue((new File("./TestData/DataForMove/from")).exists());
+        assertTrue((new File("./TestData/DataForMove/from/File1.txt")).exists());
+        List<String> sourceList = createListOfFileContent("./TestData/DataForMove/from/File1.txt");
+        FileManager.move("./TestData/DataForMove/from", "./TestData/DataForMove/to");
+        assertFalse((new File("./TestData/DataForMove/from")).exists());
+        assertFalse((new File("./TestData/DataForMove/from/File1.txt")).exists());
+        assertTrue((new File("./TestData/DataForMove/to/File1.txt")).exists());
+        assertTrue((new File("./TestData/DataForMove/to")).exists());
+        List<String> destinationList = createListOfFileContent("./TestData/DataForMove/to/File1.txt");
+        assertTrue(checkIfTwoFilesAreEqual(sourceList, destinationList));
     }
+
+    @AfterEach
+    public void after(){
+        new File("TestData/DataForCount/File1.txt").delete();
+        new File("TestData/DataForCount/File2.txt").delete();
+        new File("TestData/DataForCount/File3.txt").delete();
+        new File("TestData/DataForCount/InnerDirectory1/File4.txt").delete();
+        new File("TestData/DataForCopy/from/File1.txt").delete();
+        new File("TestData/DataForCopy/from/File2.txt").delete();
+        new File("TestData/DataForCopy/from/InnerFolder/File3.txt").delete();
+        new File("TestData/DataForCopy/to/File1.txt").delete();
+        new File("TestData/DataForCopy/to/File2.txt").delete();
+        new File("TestData/DataForCopy/to/InnerFolder/File3.txt").delete();
+        new File("TestData/DataForMove/from/File1.txt").delete();
+        new File("TestData/DataForMove/from/InnerFolder/File1.txt").delete();
+        new File("TestData/DataForMove/to/File1.txt").delete();
+        new File("TestData/DataForMove/to/InnerFolder/File1.txt").delete();
+        new File("TestData/DataForCount/InnerDirectory1/InnerDIrectory2").delete();
+        new File("TestData/DataForCount/InnerDirectory1").delete();
+        new File("TestData/DataForCount").delete();
+        new File("TestData/DataForCopy/from/InnerFolder").delete();
+        new File("TestData/DataForCopy/from").delete();
+        new File("TestData/DataForCopy/to/InnerFolder").delete();
+        new File("TestData/DataForCopy/to").delete();
+        new File("TestData/DataForCopy").delete();
+        new File("TestData/DataForMove/from/InnerFolder").delete();
+        new File("TestData/DataForMove/from").delete();
+        new File("TestData/DataForMove/to/InnerFolder").delete();
+        new File("TestData/DataForMove/to").delete();
+        new File("TestData/DataForMove").delete();
+        new File("TestData").delete();
+    }
+
+
 }
